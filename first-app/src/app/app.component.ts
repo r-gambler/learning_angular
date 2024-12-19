0765ba09683113667ff4d113f9786d130e0dd6d9
Mark A <59680128+r-gambler@users.noreply.github.com>
2024-12-18 19:47:17 -0700

    added routes and passing data through it

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
       <header class="brand-name">
@@ -13,7 +13,7 @@ import { HomeComponent } from './home/home.component';
       <img class="brand-logo" src = "/assets/logo.svg" alt="logo" aria- hidden="true" >
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>`,
   styleUrl: './app.component.css'
